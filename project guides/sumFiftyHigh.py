16c56b16dbede7ceb504f131db78f231c4d905e2
Neeraj Yalamanchili <neeraj.yalamanchili@gmail.com>
2026-05-25 08:05:16 -0500

    changed sumFiftyHigh.py

--- project guides/sumFiftyHigh.py
+++ project guides/sumFiftyHigh.py
@@ -1,7 +1,12 @@
 numbers = [32, 42, 88, 48, 72, 25, 61, 97]
 
-count = 0
+sum = 0
 
 for number in numbers:
     if number >= 50:
-        count += number
+        sum += number
+
+print("The count is ", count)
+
+for i in range(len(numbers)):
+    print(numbers[i])
